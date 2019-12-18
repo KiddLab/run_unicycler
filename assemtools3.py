bd87e21a98cb37401c14e3e950af90bc92361849
Jeffrey Kidd <jmkidd@umich.edu>
2019-12-18 14:18:03 -0500

    fix circle cmds

--- assemtools3.py
+++ assemtools3.py
@@ -400,22 +400,25 @@ def run_unicycler_assem(myData):
 #####################################################################
 def do_rotate_circle(myData):
     myData['rotatedFa'] = myData['originalAssem'] + '.rotate.fa'
-    myData['rotatedCleanFa'] = myData['originalAssem'] + '.rotate.clean.fa'
+    myData['rotatedCleanFa'] = myData['outDirBase'] + myData['name'] + '.fa'
 
     print('initial assembly:',myData['assemFa'])
     print('portion to set at start:',myData['targetFa'])
     print('make version without the target:',myData['doClean'])
+    
+    
+    print('\n*****\nNote: I am assuming there is a single circular contig and that the vector target is in the middle, not split\n*****\n')
 
     # run blat of target vs assembly, to determine what the correct orientation is
     myData['blatOutFile'] = myData['assemFa'] + '.blatTMP'
     cmd = 'blat %s %s %s' % (myData['assemFa'],myData['targetFa'],myData['blatOutFile'])
     print(cmd)
     runCMD(cmd)
-    blatLine = assemtools3.read_top_blat_line(myData['blatOutFile'])
+    blatLine = read_top_blat_line(myData['blatOutFile'])
     if blatLine['strand'] == '-':
         print('Need to make reverse complement of sequence!')
         myData['assemFa'] = myData['originalAssem'] + '.rc.fa'
-        fastaSeqs = assemtools3.read_fasta_file_to_dict(myData['originalAssem'])
+        fastaSeqs = read_fasta_file_to_dict(myData['originalAssem'])
     #    print(fastaSeqs)
         name = list(fastaSeqs.keys())[0]
         seq = fastaSeqs[name]['seq']
@@ -448,7 +451,7 @@ def do_rotate_circle(myData):
     # for now, will assume that the vector sequence is not across the circle junction -- that would be a complication
     # will do the coordinates in 1 base system, not bed/psl
     tStart = blatLine['tStart'] + 1
-    fastaSeqs = assemtools3.read_fasta_file_to_dict(myData['assemFa'])
+    fastaSeqs = read_fasta_file_to_dict(myData['assemFa'])
     name = list(fastaSeqs.keys())[0]
     newName = name.split()[0] # simplify the name
 
@@ -463,15 +466,15 @@ def do_rotate_circle(myData):
     outFile = open(myData['rotatedFa'],'w')
     outFile.write('>%s\n' % (newName))
     seq = part1 + part2
-    seq = assemtools3.add_breaks_to_line(seq,n=100)
+    seq = add_breaks_to_line(seq,n=100)
     outFile.write(seq)
     outFile.close()
 
     myData['rotatedFaBlatOutFile'] = myData['rotatedFa']+ '.blat'
     cmd = 'blat %s %s %s' % (myData['rotatedFa'],myData['targetFa'],myData['rotatedFaBlatOutFile'])
     print(cmd)
-    assemtools3.runCMD(cmd)
-    blatLine = assemtools3.read_top_blat_line(myData['rotatedFaBlatOutFile'])
+    runCMD(cmd)
+    blatLine = read_top_blat_line(myData['rotatedFaBlatOutFile'])
 
     if myData['doClean'] is True:
         print('making version without the vector!')
@@ -480,15 +483,14 @@ def do_rotate_circle(myData):
         print('q hit:',blatLine['qStart'],'-',blatLine['qEnd'])
         print('t hit:',blatLine['tStart'],'-',blatLine['tEnd'])
     
-        fastaSeqs = assemtools3.read_fasta_file_to_dict(myData['rotatedFa'])
-        name = list(fastaSeqs.keys())[0]
-        seq = fastaSeqs[name]['seq']
-    
+        fastaSeqs = read_fasta_file_to_dict(myData['rotatedFa'])
+        name = list(fastaSeqs.keys())[0]        
+        seq = fastaSeqs[name]['seq']    
         #tEnd is in bedFormat -- so can use directly, will get the next base...
         newSeq = seq[blatLine['tEnd']:]
-        newSeq = assemtools3.add_breaks_to_line(newSeq,n=100)
+        newSeq = add_breaks_to_line(newSeq,n=100)
         outFile = open(myData['rotatedCleanFa'],'w')
-        outFile.write('>%s\n' % (name))
+        outFile.write('>%s\n' % (myData['name']))
         outFile.write(newSeq)
         outFile.close()
 #####################################################################
